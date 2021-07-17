9e0df2b1f1ea0bd2acf2b555b1c9b7f73badbeba
yoonBot <yoon.ohbot@gmail.com>
2021-07-17 14:23:16 +0900

    Add files via upload

---
+++ Matrices/Multiply/matrix_multiply.c
@@ -0,0 +1,102 @@
+/* Multiplying 2 matrices
+ * @author yoonBot
+ * 06/04/2021
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Additional functions used throughout the program */
+void SquareMatrixMult(int*, int, int*, int*);
+void printMatrix(int*, int, int);
+
+/* Driver Program */
+int main(){
+    int row1, col1, row2, col2;
+    int *res, *mat1, *mat2;
+    double elapsed;
+
+    printf("Enter Matrix #1 row: ");
+    scanf("%d", &row1);
+    printf("Enter Matrix #1 col: ");
+    scanf("%d", &col1);
+
+    mat1 = (int*)malloc(row1 * col1 * sizeof(int));
+
+    for (int i = 0; i < row1; i++)
+        for (int j = 0; j < col1; j++){
+            printf("Enter element at pos: %d,%d : ", i, j);
+            scanf("%d", (mat1 + i * col1 + j));
+        }
+
+    printf("Enter Matrix #2 row: ");
+    scanf("%d", &row2);
+    while (col1 != row2){
+        printf("Error! col1 and row2 sizes must be equivalent!\n");
+        scanf("%d", &row2);
+    }
+    printf("Enter Matrix #2 col: ");
+    scanf("%d", &col2);   
+    
+    mat2 = (int*)malloc(row2 * col2 * sizeof(int));
+
+    for (int i = 0; i < row2; i++)
+        for (int j = 0; j < col2; j++){
+            printf("Enter element at pos: %d,%d : ", i, j);
+            scanf("%d", (mat2 + i * col2 + j));
+        }
+    
+    // if a x b and b x c, then a x c for final. 
+    res = (int*)malloc(row1 * col2 * sizeof(int));
+    
+    /* Display input */
+    printf("Matrix #1:\n");
+    printMatrix(mat1, row1, col1);
+    printf("Matrix #2:\n");
+    printMatrix(mat2, row2, col2);
+    printf("Result Matrix:\n");
+    
+    /* Start Clock */
+    clock_t start = clock();
+        // we only need either row1 or col2, since both sizes must be equal anyways
+    SquareMatrixMult(mat1, row1, mat2, res);
+    printMatrix(res, row1, col2);
+
+    /* End Timer */
+    elapsed += (double) (clock() - start);
+    elapsed /= CLOCKS_PER_SEC;
+    
+    /* print */
+    printf("Computation Completed in %.6lf seconds\n", elapsed);
+    
+    free(mat1);
+    free(mat2);
+    free(res);
+
+    return 0;
+}
+
+/* Multiplying Two Matrixes */
+void SquareMatrixMult(int *mat1, int row1, int *mat2, int *res){
+    int i, j, k;
+    for (i = 0; i < row1; i++)
+        for (j = 0; j < row1; j++){
+            //res[i][j] = 0;
+            *(res + i * row1 + j) = 0;
+            for (k = 0; k < row1; k++)
+                //res[i][j] += mat1[i][k] * mat2[k][j];
+                *(res + i * row1 + j) += *(mat1 + i * row1 + k) * *(mat2 + k * row1 + j);
+        }
+}
+
+/* Print Matrixes */
+void printMatrix(int *mat, int row, int col){
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++)
+            printf("%d ", *(mat + i * col + j));
+        printf("\n");
+    }
+}
+
+
---
+++ Matrices/Multiply/matrix_multiply.cpp
@@ -0,0 +1,104 @@
+/* Multiplying 2 matrices
+ * @author yoonBot
+ * 06/04/2021
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+/* Additional functions used throughout the program */
+void SquareMatrixMult(int*, int, int*, int*);
+void printMatrix(int*, int, int);
+
+/* Driver Program */
+int main(){
+    int row1, col1, row2, col2;
+    int *res, *mat1, *mat2;
+    double elapsed;
+
+    cout << "Enter Matrix #1 row: ";
+    cin >> row1;
+    cout << "Enter Matrix #1 col: ";
+    cin >> col1;
+
+    mat1 = new int[row1 * col1];
+
+    for (int i = 0; i < row1; i++)
+        for (int j = 0; j < col1; j++){
+            cout << "Enter element at pos: " << i << "," << j << " : ";
+            scanf("%d", (mat1 + i * col1 + j));
+        }
+
+    cout << "Enter Matrix #2 row: ";
+    cin >> row2;
+    cout << "Enter Matrix #2 col: ";
+    cin >> col2;
+    if (col1 != row2){
+        cout << "Error! Cannot Multiply Matrixes!\n";
+        exit(3);
+    }
+
+    mat2 = new int[row2 * col2];
+
+    for (int i = 0; i < row2; i++)
+        for (int j = 0; j < col2; j++){
+            int element;
+            cout << "Enter element at pos: " << i << "," << j << " : ";
+            cin >> element;
+            *(mat2 + i * col2 + j) = element;
+        }
+
+    // if a x b and b x c, then a x c for final
+    res = new int[row1 * col2 + 2];
+
+    /* Display input */
+    cout << "Matrix #1:\n";
+    printMatrix(mat1, row1, col1);
+    cout << "Matrix #2:\n";
+    printMatrix(mat2, row2, col2);
+    cout << "Result Matrix:\n";
+
+    /* Start Clock */
+    clock_t start = clock();
+        // we only need either row1 or col2, since both sizes must be equal anyways
+    SquareMatrixMult(mat1, row1, mat2, res);
+    printMatrix(res, row1, col2);
+
+    /* End Timer */
+    elapsed += (double) (clock() - start);
+    elapsed /= CLOCKS_PER_SEC;
+
+    /* print */
+    printf("Computation Completed in %.6lf seconds\n", elapsed);
+
+    delete [] mat1;
+    delete [] mat2;
+    delete [] res;
+
+    return 0;
+}
+
+/* Multiplying Two Matrixes */
+void SquareMatrixMult(int *mat1, int row1, int *mat2, int *res){
+    int i, j, k;
+    for (i = 0; i < row1; i++)
+        for (j = 0; j < row1; j++){
+            //res[i][j] = 0;
+            *(res + i * row1 + j) = 0;
+            for (k = 0; k < row1; k++)
+                //res[i][j] += mat1[i][k] * mat2[k][j];
+                *(res + i * row1 + j) += *(mat1 + i * row1 + k) * *(mat2 + k * row1 + j);
+        }
+}
+
+/* Print Matrixes */
+void printMatrix(int *mat, int row, int col){
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++)
+            printf("%d ", *(mat + i * col + j));
+        cout << "\n";
+    }
+}
